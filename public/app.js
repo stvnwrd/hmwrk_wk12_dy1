2fcf548debf08cfb6dfdbcfca383980a058d6214
Steven Ward <34653526+stvnwrd@users.noreply.github.com>
2018-04-09 22:48:14 +0100

    Refactor to make it 'easy' to add a new cat

--- public/app.js
+++ public/app.js
@@ -1,32 +1,77 @@
-const app = function(){
+// const app = function(){
+//
+//   let catUl = document.createElement("ul");
+//   catUl.classList.add("cat");
+//
+//   let name = document.createElement("li");
+//   name.innerText = "Name: Mummy's Little Darling";
+//
+//   let favFood = document.createElement("li");
+//   favFood.innerText = "Favourite Food: Dormice, little birds, assorted other innocents";
+//
+//   let photo = document.createElement("li");
+//   photo.innerHTML = '<img width="500" src="../hangover-237197_640.jpg">';
+//
+//   catUl.appendChild(name);
+//
+//   catUl.appendChild(favFood);
+//
+//   catUl.appendChild(photo);
+//
+//   let cats = document.querySelector("#cats");
+//   cats.appendChild(catUl);
+//
+// };
+//
+// window.onload = app;
 
-  let catUl = document.createElement("ul");
-  catUl.classList.add("cat");
 
-  let name = document.createElement("li");
-  name.innerText = "Name: Mummy's Little Darling";
-
-
-  let favFood = document.createElement("li");
-  favFood.innerText = "Favourite Food: Dormice, little birds, assorted other innocents"
-
-  let photo = document.createElement("li");
-  photo.innerHTML = '<img width="500" src="../hangover-237197_640.jpg">';
-
-
-
-
-  catUl.appendChild(name);
-
-  catUl.appendChild(favFood);
-
-  catUl.appendChild(photo);
-
-
-  let cats = document.querySelector("#cats");
-  cats.appendChild(catUl);
-
-    // debugger;
+var createCatUl = function(){
+    let catUl = document.createElement("ul");
+    catUl.classList.add("cat");
+    return catUl;
 };
 
-window.onload = app;
+var createName = function(cat){
+    let name = document.createElement("li");
+    name.innerText = cat;
+    return name;
+};
+
+var createFavFood = function(food){
+    let favFood = document.createElement("li");
+    favFood.innerText = food;
+    return favFood;
+};
+
+var createPhoto = function(pic){
+    let photo = document.createElement("li");
+    photo.innerHTML = pic;
+    return photo;
+};
+
+
+var appendElements = function(catUl, name, favFood, photo){
+  catUl.appendChild(name);
+  catUl.appendChild(favFood);
+  catUl.appendChild(photo);
+  let cats = document.querySelector("#cats");
+  cats.appendChild(catUl);
+};
+
+
+
+var addCat = function(cat, food, pic) {
+  var catUl = createCatUl();
+  var name = createName(cat);
+  var favFood = createFavFood(food);
+  var photo = createPhoto(pic);
+
+  appendElements(catUl, name, favFood, photo);
+}
+
+
+
+window.onload = function(){
+  addCat("Name: Mummy's Little Darling", 'Favourite Food: Dormice, little birds, assorted other innocents', '<img width="500" src="../hangover-237197_640.jpg">');
+}
